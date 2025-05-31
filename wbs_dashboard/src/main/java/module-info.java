418a355a27acf4f917cdfa6d1d344cd80e7cc1aa
zaruous <zaruous@naver.com>
2025-06-01 02:15:27 +0900

    add 마리아db lib

--- wbs_dashboard/src/main/java/module-info.java
+++ wbs_dashboard/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.kyj.fx.wsb.dashboard {
 	requires java.sql;
 	requires java.desktop;
 	requires javafx.swing;
+	requires org.mariadb.jdbc; // MariaDB JDBC 드라이버 모듈
     opens org.kyj.fx.wbs.dashboard to javafx.fxml;
     exports org.kyj.fx.wbs.dashboard;
 }
